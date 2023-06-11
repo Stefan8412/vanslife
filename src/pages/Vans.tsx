7b2cfd9425bc772857d004554ecb30bbcd8ec236
Stefan Hancar <stefan@hancar.sk>
2023-06-11 07:25:34 +0200

    add css

--- src/pages/Vans.tsx
+++ src/pages/Vans.tsx
@@ -1,10 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Vans() {
+  const [vans, setVans] = useState<any[]>([]);
+
+  //console.log(name);
   useEffect(() => {
     fetch("/api/vans")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => setVans(data.vans));
   }, []);
-  return <h1>Vans page</h1>;
+  const vanElements = vans.map((van) => (
+    <div key={van.id} className="van-title">
+      <img src={van.imageUrl} />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>
+          ${van.price}
+          <span>/day</span>
+        </p>
+      </div>
+      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </div>
+  ));
+  return (
+    <div className="van-list-container">
+      <div className="van-list">{vanElements}</div>
+    </div>
+  );
 }
